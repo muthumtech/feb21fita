3319707c0ec8f913dc8ffd4aa099db53c6e81a38
muthumtech <muthumtech@gmail.com>
2026-02-21 19:57:22 +0530

    front

---
+++ tsd/f2.js
@@ -0,0 +1,21 @@
+// A simple script to add interactivity to your HTML
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.querySelector('h1');
+    
+    // Change text when the heading is clicked
+    title.addEventListener('click', () => {
+        title.style.color = 'blue';
+        title.innerText = 'JS is Working!';
+        console.log('The title was clicked!');
+    });
+});
+
+// A basic function example
+function greetUser(name) {
+    alert('Welcome to the site, ' + name + '!');
+}
+
+// A basic function2 example
+function greetUser1(name1) {
+    alert('Welcome to the site, ' + name1 + '!');
+}
